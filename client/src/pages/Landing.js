0db4957f480df914e2f0edf25cd90c63282e6201
Gabriel Cánepa <gacanepa@gmail.com>
2022-05-17 07:53:06 -0300

    Added landing page

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
+import { INTRO_TEXT, SITE_TITLE, LOGIN, REGISTER } from '../common/constants/pages';
+
+const Landing = () => (
+  <main>
+    <nav>
+      <img src={logo} alt={SITE_TITLE} className="logo" />
+      <div className="container page">
+        <div className="info">
+          <h1>
+            <span>{SITE_TITLE}</span> for organizations
+          </h1>
+          <p>{INTRO_TEXT}</p>
+          <button className="btn btn-hero">
+            {LOGIN} / {REGISTER}
+          </button>
+        </div>
+        <img src={main} alt="" className="img main-img" />
+      </div>
+    </nav>
+  </main>
+);
+
+export default Landing;
