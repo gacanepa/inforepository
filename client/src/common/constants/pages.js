23847107664f0c51a4dd0b5940873875104d41f0
Gabriel Cánepa <gacanepa@gmail.com>
2022-05-17 07:52:41 -0300

    Created constants file for pages

---
+++ client/src/common/constants/pages.js
@@ -0,0 +1,5 @@
+export const SITE_TITLE = 'Information Repository';
+export const LANDING = 'Landing Page';
+export const INTRO_TEXT = 'Dreamcatcher butcher knausgaard aute skateboard chartreuse shabby chic chambray man braid. Et put a bird on it chambray health goth.';
+export const LOGIN = 'Login';
+export const REGISTER = 'Register';
