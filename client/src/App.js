c66530edc37b50cb9bfd662e58a0a5bc013ff6ca
Gabriel Cánepa <gacanepa@gmail.com>
2022-05-17 07:53:31 -0300

    Updated app with new component

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,11 @@
-const App = () => <h1>Info Repository</h1>;
+import Landing from './pages/Landing';
+import { SITE_TITLE } from './common/constants/pages';
+
+const App = () => (
+  <div>
+    <h1>{SITE_TITLE}</h1>
+    <Landing />
+  </div>
+);
 
 export default App;
