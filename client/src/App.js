9cf5ab2fe7fdfc99ef0fd4245125589d9b413e19
Gabriel Cánepa <gacanepa@gmail.com>
2022-05-30 10:57:51 -0300

    Removed site title from main app file

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import Landing from './pages/Landing';
-import { SITE_TITLE } from './common/constants/pages';
 
 const App = () => (
   <div>
-    <h1>{SITE_TITLE}</h1>
     <Landing />
   </div>
 );
