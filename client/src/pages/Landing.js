1379047f65c559342a88325b8f2467849607b77d
Gabriel Cánepa <gacanepa@gmail.com>
2022-05-30 10:58:19 -0300

    Implemented wrapper for the landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage'
 import { INTRO_TEXT, SITE_TITLE, LOGIN, REGISTER } from '../common/constants/pages';
 
 const Landing = () => (
-  <main>
+  <Wrapper>
     <nav>
       <img src={logo} alt={SITE_TITLE} className="logo" />
-      <div className="container page">
-        <div className="info">
-          <h1>
-            <span>{SITE_TITLE}</span> for organizations
-          </h1>
-          <p>{INTRO_TEXT}</p>
-          <button className="btn btn-hero">
-            {LOGIN} / {REGISTER}
-          </button>
-        </div>
-        <img src={main} alt={SITE_TITLE} className="img main-img" />
-      </div>
     </nav>
-  </main>
+    <div className="container page">
+      <div className="info">
+        <h1>
+          <span>{SITE_TITLE}</span> for organizations
+        </h1>
+        <p>{INTRO_TEXT}</p>
+        <button className="btn btn-hero">
+          {LOGIN} / {REGISTER}
+        </button>
+      </div>
+      <img src={main} alt={SITE_TITLE} className="img main-img" />
+    </div>
+  </Wrapper>
 );
 
 export default Landing;
