010860389c75fb181701be5b59098ce0cbe6aea0
Gabriel Cánepa <gacanepa@gmail.com>
2022-06-16 20:18:28 -0300

    Set up the initial generic routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Landing from './pages/Landing';
 
 const App = () => (
-  <div>
-    <Landing />
-  </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<div>Dashboard</div>} />
+      <Route path="/register" element={<div>Register</div>} />
+      <Route path="/landing" element={<Landing />} />
+      <Route path="*" element={<div>Error</div>} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
