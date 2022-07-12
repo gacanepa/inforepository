71be6e388e2133fba79bb2d5beb40f4baa446297
Gabriel Cánepa <gacanepa@gmail.com>
2022-07-12 12:24:13 -0300

    Added new ESLint rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,22 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  root: true,
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'arrow-parens': ['warn', 'as-needed'],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'comma-dangle': ['warn', 'only-multiline'],
+    'no-unused-vars': ['warn', { args: 'all', argsIgnorePattern: '^_' }],
+  },
+};
