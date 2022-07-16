b826ffa063b37b1abe87fce05ae12292e2955ce9
Gabriel Cánepa <gacanepa@gmail.com>
2022-07-15 21:03:22 -0300

    Removed request data from the response

--- middleware/notFoundMiddleware.js
+++ middleware/notFoundMiddleware.js
@@ -1,5 +1,5 @@
 const notFoundMiddleware = (req, res) => {
-  res.status(404).send(`Route ${req.url} not found`);
+  res.status(404).send('Route does not exist');
 };
 
 export default notFoundMiddleware;
