c212213f47651d9c3791bbb874b76951d65a7ba6
Gabriel Cánepa <gacanepa@gmail.com>
2022-07-22 21:24:47 -0300

    Created constants file for models

---
+++ models/constants.js
@@ -0,0 +1,9 @@
+export const IS_REQUIRED = 'is required';
+export const NOT_VALID = 'is not valid';
+export const MIN_LENGTH = 'must be at least';
+export const MAX_LENGTH = 'must be at most';
+export const CHARACTERS = 'characters long';
+export const MUST_BE_UNIQUE = 'must be unique';
+export const STRONG_PASSWORD = 'The minimum length is 8 characters. It must contain at least one number and a symbol, one uppercase and one lowercase letter.';
+export const FIRST_NAME = 'First name';
+export const LAST_NAME = 'Last name';
