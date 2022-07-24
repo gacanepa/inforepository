6dd5d69f2646f92515f8226191cf2e997b710f7b
Gabriel Cánepa <gacanepa@gmail.com>
2022-07-23 22:30:31 -0300

    Handled error for email in use

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,17 @@
-const register = async (_req, res) => {
-  res.status(200).send('register');
+import { StatusCodes } from 'http-status-codes';
+import User from '../models/User.js';
+import { EMAIL_IN_USE } from './constants.js';
+
+const { CREATED, INTERNAL_SERVER_ERROR } = StatusCodes;
+
+const register = async (req, res) => {
+  const { email } = req.body;
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new Error(EMAIL_IN_USE);
+  }
+  const user = await User.create(req.body);
+  res.status(CREATED).json({ user });
 };
 
 const login = async (_req, res) => {
