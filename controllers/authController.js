c49cab56c45d43fc5ef0db8b043bb9bb0fa3879a
Gabriel Cánepa <gacanepa@gmail.com>
2022-07-26 16:48:21 -0300

    Implemented errors at the controller level

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/User.js';
-import { EMAIL_IN_USE } from './constants.js';
+import { BadRequestError } from '../errors/index.js';
+import { EMAIL_IN_USE, PLEASE_PROVIDE_ALL_VALUES } from './constants.js';
 import handleNullUndefined from '../utilities/handleNullUndefined.js';
 
 const { CREATED } = StatusCodes;
@@ -14,11 +15,16 @@ const register = async (req, res) => {
     location
   } = req.body;
 
+  if (!firstName || !email || !password || !lastName) {
+    throw new BadRequestError(PLEASE_PROVIDE_ALL_VALUES);
+  }
+
   // Sanitize user-provided input to prevent NoSQL injection
   const userAlreadyExists = await User.findOne({ email: String(email) });
   if (userAlreadyExists) {
-    throw new Error(EMAIL_IN_USE);
+    throw new BadRequestError(EMAIL_IN_USE);
   }
+
   const user = await User.create({
     firstName: handleNullUndefined(firstName),
     email: handleNullUndefined(email),
@@ -26,6 +32,7 @@ const register = async (req, res) => {
     lastName: handleNullUndefined(lastName),
     location: handleNullUndefined(location),
   });
+
   res.status(CREATED).json({ user });
 };
 
