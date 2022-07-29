69ccbf58ce125387fa8f365ed1753396a24e3f3b
Gabriel Cánepa <gacanepa@gmail.com>
2022-07-29 10:42:31 -0300

    Added salt rounds as constant

--- models/constants.js
+++ models/constants.js
@@ -7,3 +7,4 @@ export const MUST_BE_UNIQUE = 'must be unique';
 export const STRONG_PASSWORD = 'The minimum length is 8 characters. It must contain at least one number and a symbol, one uppercase and one lowercase letter.';
 export const FIRST_NAME = 'First name';
 export const LAST_NAME = 'Last name';
+export const SALT_ROUNDS = 10;
