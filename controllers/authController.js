cd9acc75006f73e4106535996f9ab3b426cec127
Gabriel Cánepa <gacanepa@gmail.com>
2022-08-16 09:03:36 -0300

    Implemented login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/User.js';
-import { BadRequestError } from '../errors/index.js';
-import { EMAIL_IN_USE, PLEASE_PROVIDE_ALL_VALUES } from './constants.js';
+import { BadRequestError, UnauthenticatedError } from '../errors/index.js';
+import { EMAIL_IN_USE, PLEASE_PROVIDE_ALL_VALUES, INVALID_CREDENTIALS } from './constants.js';
 import handleNullUndefined from '../utilities/handleNullUndefined.js';
 
 const { CREATED, OK } = StatusCodes;
@@ -47,8 +47,31 @@ const register = async (req, res) => {
   });
 };
 
-const login = async (_req, res) => {
-  res.status(OK).send('login');
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError(PLEASE_PROVIDE_ALL_VALUES);
+  }
+
+  // Add '+password' to include the password in the response
+  // (by default it is not present because of the model definition)
+  const existingUser = await User.findOne({ email: String(email) }).select('+password');
+  if (!existingUser) {
+    throw new UnauthenticatedError(INVALID_CREDENTIALS);
+  }
+
+  const isPasswordValid = await existingUser.comparePassword(password);
+
+  if (!isPasswordValid) {
+    throw new UnauthenticatedError(INVALID_CREDENTIALS);
+  }
+
+  // Make password undefined to remove it from the response
+  existingUser.password = undefined;
+
+  const token = existingUser.createJWT();
+  res.status(OK).json({ existingUser, token });
 };
 
 const updateUser = async (_req, res) => {
