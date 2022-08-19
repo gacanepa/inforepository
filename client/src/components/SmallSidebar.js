ba8fa76ed7a299ec91db1d4547242edc78146969
Gabriel Cánepa <gacanepa@gmail.com>
2022-08-19 17:24:22 -0300

    Extracted nav links as a separate component

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { FaTimes } from 'react-icons/fa';
-import { NavLink } from 'react-router-dom';
 import SmallSidebarWrapper from '../assets/wrappers/SmallSidebarWrapper';
 import { useAppContext } from '../context/AppContext';
-import links from '../utilities/links';
 import Logo from './Logo';
+import NavLinks from './NavLinks';
 
 const SmallSidebar = () => {
   const { showSidebar, toggleSidebar } = useAppContext();
@@ -13,7 +12,7 @@ const SmallSidebar = () => {
     <SmallSidebarWrapper>
       <div className={
         showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
-        }
+      }
       >
         <div className="content">
           <button
@@ -26,22 +25,7 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">
-            {links.map(link => {
-              const { icon, id, path, text } = link;
-              return (
-                <NavLink
-                  key={id}
-                  to={path}
-                  className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}
-                  onClick={toggleSidebar}
-                >
-                  <span className="icon">{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
-          </div>
+          <NavLinks toggleSidebar={toggleSidebar} />
         </div>
       </div>
     </SmallSidebarWrapper>
