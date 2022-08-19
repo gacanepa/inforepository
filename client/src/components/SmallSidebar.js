e2cdbb9b3179d1f0ae0fcfb3b7c1bf1e14cf0783
Gabriel Cánepa <gacanepa@gmail.com>
2022-08-19 01:49:00 -0300

    Set up links in the small sidebar

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
 import SmallSidebarWrapper from '../assets/wrappers/SmallSidebarWrapper';
+import { useAppContext } from '../context/AppContext';
+import links from '../utilities/links';
+import Logo from './Logo';
 
-const SmallSidebar = () => (
-  <SmallSidebarWrapper>
-    <h4>Small Sidebar</h4>
-  </SmallSidebarWrapper>
-);
+const SmallSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
+
+  return (
+    <SmallSidebarWrapper>
+      <div className={
+        showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className="content">
+          <button
+            type="button"
+            className="close-btn"
+            onClick={toggleSidebar}
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            {links.map(link => {
+              const { icon, id, path, text } = link;
+              return (
+                <NavLink
+                  key={id}
+                  to={path}
+                  className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}
+                  onClick={toggleSidebar}
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </SmallSidebarWrapper>
+  );
+};
 
 export default SmallSidebar;
