ab81084632d658aa6ed9f3a0db687af60bbbd40d
Gabriel Cánepa <gacanepa@gmail.com>
2022-08-22 15:03:05 -0300

    Updated notFoundMiddleware

--- middleware/notFoundMiddleware.js
+++ middleware/notFoundMiddleware.js
@@ -1,5 +1,11 @@
+import { StatusCodes } from 'http-status-codes';
+import { SOMETHING_WENT_WRONG } from './constants.js';
+
+const { NOT_FOUND } = StatusCodes;
+
 const notFoundMiddleware = (_req, res) => {
-  res.status(404).send('Route does not exist');
+  // Respond with a generic message to avoid revealing too much details
+  res.status(NOT_FOUND).send({ message: SOMETHING_WENT_WRONG });
 };
 
 export default notFoundMiddleware;
