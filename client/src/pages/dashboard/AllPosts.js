4c1d480e9ffd9a00c0dc9cb1c58eaa574dfbe4ab
Gabriel Cánepa <gacanepa@gmail.com>
2022-08-27 22:24:56 -0300

    Changed heading size

--- client/src/pages/dashboard/AllPosts.js
+++ client/src/pages/dashboard/AllPosts.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ALL_POSTS } from '../../common/constants/pages';
 
-const AllPosts = () => <h1>{ALL_POSTS}</h1>;
+const AllPosts = () => <h3>{ALL_POSTS}</h3>;
 
 export default AllPosts;
--- client/src/pages/dashboard/Stats.js
+++ client/src/pages/dashboard/Stats.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { STATS } from '../../common/constants/pages';
 
-const Stats = () => <h1>{STATS}</h1>;
+const Stats = () => <h3>{STATS}</h3>;
 
 export default Stats;
