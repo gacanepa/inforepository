fbc3cb3215d77c5fca32d6a6f0c44257e2518c05
Gabriel Cánepa <gacanepa@gmail.com>
2022-08-31 10:39:45 -0300

    Made error message more generic

--- middleware/constants.js
+++ middleware/constants.js
@@ -1,5 +1,5 @@
 export const SOMETHING_WENT_WRONG = 'Something went wrong. Please try again later and contact your system administrator if the issue persists.';
 export const VALIDATION_ERROR = 'ValidationError';
 export const DUPLICATE_KEY_ERROR = 11000;
-export const NOT_AUTHORIZED = 'You are not authorized to perform this action';
+export const NOT_AUTHORIZED = 'You are not authorized to perform this action or access this resource';
 export const BEARER = 'Bearer';
