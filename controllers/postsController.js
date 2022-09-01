63adc4de5f0a884fa58bcb74c37b7b7a6410e921
Gabriel Cánepa <gacanepa@gmail.com>
2022-09-01 10:39:17 -0300

    Set up the create post path

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,10 +1,22 @@
+import Post from '../models/Post.js';
 import { StatusCodes } from 'http-status-codes';
+import { BadRequestError, UnauthorizedError } from '../errors/index.js';
+import { PLEASE_PROVIDE_ALL_VALUES } from './constants.js'
 
 // Get status codes
-const { OK } = StatusCodes;
+const { OK, CREATED } = StatusCodes;
 
-const createPost = async (_req, res) => {
-  res.status(OK).send('createPost');
+const createPost = async (req, res) => {
+  const { title, content } = req.body;
+
+  if (!title || !content) {
+    throw new BadRequestError(PLEASE_PROVIDE_ALL_VALUES);
+  }
+
+  req.body.createdBy = req.user.userId;
+  const post = await Post.create(req.body);
+
+  res.status(CREATED).json({ post });
 };
 
 const deletePost = async (_req, res) => {
