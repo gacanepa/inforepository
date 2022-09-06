fce78302902f2e644a46ccabb3d25d8d3921ee6d
Gabriel Cánepa <gacanepa@gmail.com>
2022-09-06 09:48:14 -0300

    Addressed vulnerability and code smell

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,20 +1,36 @@
 import Post from '../models/Post.js';
 import { StatusCodes } from 'http-status-codes';
-import { BadRequestError, UnauthorizedError } from '../errors/index.js';
+import { BadRequestError } from '../errors/index.js';
 import { PLEASE_PROVIDE_ALL_VALUES } from './constants.js'
+import handleNullUndefined from '../utilities/handleNullUndefined.js';
 
 // Get status codes
 const { OK, CREATED } = StatusCodes;
 
 const createPost = async (req, res) => {
-  const { title, content } = req.body;
+  const {
+    title,
+    content,
+    importance,
+    classification,
+    type,
+    dueDate
+  } = req.body;
 
   if (!title || !content) {
     throw new BadRequestError(PLEASE_PROVIDE_ALL_VALUES);
   }
 
-  req.body.createdBy = req.user.userId;
-  const post = await Post.create(req.body);
+  const createdBy = req.user.userId;
+  const post = await Post.create({
+    title: handleNullUndefined(title),
+    content: handleNullUndefined(content),
+    importance: handleNullUndefined(importance),
+    classification: handleNullUndefined(classification),
+    type: handleNullUndefined(type),
+    dueDate: handleNullUndefined(dueDate),
+    createdBy: handleNullUndefined(createdBy),
+  });
 
   res.status(CREATED).json({ post });
 };
