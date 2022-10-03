9b2c5e8daf0aa531b12c6b5a346e73ded124197c
Gabriel Cánepa <gacanepa@gmail.com>
2022-10-03 20:10:23 -0300

    Created new search container

---
+++ client/src/components/SearchContainer.js
@@ -0,0 +1,12 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+
+const SearchContainer = () => {
+  return (
+    <div>
+      <h1>SearchContainer</h1>
+    </div>
+  );
+};
+
+export default SearchContainer;
