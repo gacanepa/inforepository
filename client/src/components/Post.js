3ea5a212bc21c997b20802ef21931ce760a3bce8
Gabriel Cánepa <gacanepa@gmail.com>
2022-10-03 22:45:33 -0300

    Displayed post title and creation date

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-// eslint-disable-next-line react/prop-types
-const Post = ({ title }) => (
-  <div>{title}</div>
-);
+const Post = ({ title, createdAt }) => {
+  const postDate = new Date(createdAt).toLocaleDateString('en-US', {
+    dateStyle: 'medium',
+  });
+  return (
+    <div>
+      <h4>{title}</h4>
+      <h5>{postDate}</h5>
+    </div>
+  );
+};
+
+Post.propTypes = {
+  title: PropTypes.string.isRequired,
+  createdAt: PropTypes.string.isRequired,
+};
 
 export default Post;
