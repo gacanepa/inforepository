6650968a5be51a89096691a029432bde46cdef5e
Gabriel Cánepa <gacanepa@gmail.com>
2022-10-04 09:49:46 -0300

    Showed post with title, author, and importance

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,20 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FaBookOpen, FaLock, FaListUl, FaPen } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { useAppContext } from '../context/AppContext';
+import PostWrapper from '../assets/wrappers/PostWrapper';
+import PostInfo from './PostInfo';
 
-const Post = ({ title, createdAt }) => {
+const Post = ({
+  title,
+  importance,
+  classification,
+  type,
+  content,
+  createdBy,
+  createdAt
+}) => {
+  const { setEditPost, deletePost } = useAppContext();
+  const { firstName, lastName } = createdBy;
+
   const postDate = new Date(createdAt).toLocaleDateString('en-US', {
     dateStyle: 'medium',
   });
   return (
-    <div>
-      <h4>{title}</h4>
-      <h5>{postDate}</h5>
-    </div>
+    <PostWrapper>
+      <header>
+        <div className="main-icon">{importance.charAt(0)}</div>
+        <div className="info">
+          <h5>{title}</h5>
+          <p>{`${firstName} ${lastName}`}</p>
+        </div>
+      </header>
+    </PostWrapper>
   );
 };
 
 Post.propTypes = {
   title: PropTypes.string.isRequired,
+  importance: PropTypes.string.isRequired,
+  classification: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  createdBy: PropTypes.string.isRequired,
   createdAt: PropTypes.string.isRequired,
 };
 
