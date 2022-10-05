358bb346d9e61e198f1e657ee9cddd7be7d529ae
Gabriel Cánepa <gacanepa@gmail.com>
2022-10-05 12:02:06 -0300

    Implemented icons and text as post info

--- client/src/components/PostInfo.js
+++ client/src/components/PostInfo.js
@@ -1,10 +1,17 @@
-/* eslint-disable arrow-body-style */
 import React from 'react';
+import PropTypes from 'prop-types';
+import PostInfoWrapper from '../assets/wrappers/PostInfoWrapper';
 
-const PostInfo = () => {
-  return (
-    <div>PostInfo</div>
-  );
+const PostInfo = ({ icon, text }) => (
+  <PostInfoWrapper>
+    <span className="icon">{icon}</span>
+    <span className="text">{text}</span>
+  </PostInfoWrapper>
+);
+
+PostInfo.propTypes = {
+  icon: PropTypes.element.isRequired,
+  text: PropTypes.string.isRequired,
 };
 
 export default PostInfo;
