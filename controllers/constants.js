f2113255e959f9d7f786b32ffd81e486c703f332
Gabriel Cánepa <gacanepa@gmail.com>
2022-10-13 18:31:53 -0300

    Added new constants

--- controllers/constants.js
+++ controllers/constants.js
@@ -2,3 +2,5 @@ export const EMAIL_IN_USE = 'Email already in use';
 export const PLEASE_PROVIDE_ALL_VALUES = 'Please provide all values';
 export const INVALID_CREDENTIALS = 'Invalid credentials';
 export const ACCOUNT_DOES_NOT_EXIST = 'Invalid account';
+export const NO_POST_FOUND = 'No post found with id:';
+export const NOT_AUTHORIZED_TO_ACCESS_ROUTE = 'Not authorized to access this route';
