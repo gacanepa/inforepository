35d7adacca25e6ca6d725c6a6e304f7aea941e69
Gabriel Cánepa <gacanepa@gmail.com>
2022-10-21 17:28:49 -0300

    Added login, register, and months

---
+++ client/src/assets/locales/es/translation.js
@@ -0,0 +1,20 @@
+const SPA = {
+  MONTHS: {
+    1: 'Ene',
+    2: 'Feb',
+    3: 'Mar',
+    4: 'Abr',
+    5: 'May',
+    6: 'Jun',
+    7: 'Jul',
+    8: 'Ago',
+    9: 'Sep',
+    10: 'Oct',
+    11: 'Nov',
+    12: 'Dic',
+  },
+  REGISTER: 'Registrarse',
+  LOGIN: 'Iniciar sesión',
+};
+
+export default SPA;
