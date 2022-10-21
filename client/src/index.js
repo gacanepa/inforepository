be7b412e1e8cd99d0e4d734652f4558dd9e9d263
Gabriel Cánepa <gacanepa@gmail.com>
2022-10-21 17:02:03 -0300

    Added support for internationalization

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import './index.css';
 import App from './App';
 import { AppProvider } from './context/AppContext';
 
+import './i18n';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
