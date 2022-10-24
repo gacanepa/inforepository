4afb05b238c4cbc375adf9be92b69d4a1c324f08
Gabriel Cánepa <gacanepa@gmail.com>
2022-10-24 15:20:37 -0300

    Replaced constants with translations (Landing)

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,37 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useTranslationContext } from '../context/TranslationContext';
 import main from '../assets/images/main.svg';
 import LandingPageWrapper from '../assets/wrappers/LandingPageWrapper';
 import { Logo } from '../components';
-import {
-  INTRO_TEXT,
-  SITE_TITLE,
-  LOGIN,
-  REGISTER,
-} from '../common/constants/pages';
 
-const Landing = () => (
-  <LandingPageWrapper>
-    <nav>
-      <Logo />
-    </nav>
-    <div className="container page">
-      <div className="info">
-        <h1>
-          <span>{SITE_TITLE}</span>
-          <span> </span>
-          for organizations
-        </h1>
-        <p>{INTRO_TEXT}</p>
-        <Link to="/register" className="btn btn-hero">
-          {LOGIN}
-          /
-          {REGISTER}
-        </Link>
+const Landing = () => {
+  const {
+    INTRO_TEXT,
+    SITE_TITLE,
+    LOGIN,
+    REGISTER,
+  } = useTranslationContext();
+
+  return (
+    <LandingPageWrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            <span>{SITE_TITLE}</span>
+            <span> </span>
+            for organizations
+          </h1>
+          <p>{INTRO_TEXT}</p>
+          <Link to="/register" className="btn btn-hero">
+            {LOGIN}
+            /
+            {REGISTER}
+          </Link>
+        </div>
+        <img src={main} alt={SITE_TITLE} className="img main-img" />
       </div>
-      <img src={main} alt={SITE_TITLE} className="img main-img" />
-    </div>
-  </LandingPageWrapper>
-);
+    </LandingPageWrapper>
+  );
+};
 
 export default Landing;
