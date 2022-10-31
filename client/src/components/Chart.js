58d03ec83b0a5875f9cd75e35309aae174ca35b5
Gabriel Cánepa <gacanepa@gmail.com>
2022-10-31 12:01:08 -0300

    Updated the chart component

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,4 +1,7 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/require-default-props */
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
   BarChart,
   Bar,
@@ -8,9 +11,47 @@ import {
   Tooltip,
   ResponsiveContainer,
 } from 'recharts';
+import { useTranslationContext } from '../context/TranslationContext';
 
-const Chart = () => (
-  <div>Chart</div>
-);
+const Chart = ({ data }) => {
+  const { MONTH, COUNT } = useTranslationContext();
+
+  const renderTooltip = active => {
+    const { active: isActive, payload, label } = active;
+    if (isActive) return `${label}: ${payload[0].value}`;
+    return null;
+  };
+
+  return (
+    <ResponsiveContainer width="100%" height={500}>
+      <BarChart
+        data={data}
+        margin={{
+          top: 50,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" label={{ value: MONTH, position: 'insideBottom' }} />
+        <YAxis
+          allowDecimals={false}
+          label={{
+            value: COUNT,
+            angle: -90,
+            position: 'insideLeft'
+          }}
+        />
+        <Tooltip cursor={false} content={renderTooltip} />
+        <Bar dataKey="count" fill="#2cb1bc" barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+Chart.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({
+    count: PropTypes.number.isRequired,
+    date: PropTypes.string.isRequired,
+  })).isRequired,
+};
 
 export default Chart;
