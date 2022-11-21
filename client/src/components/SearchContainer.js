27cfa6122628d20b5db0e2f16f2cc99df2f15e8f
Gabriel Cánepa <gacanepa@gmail.com>
2022-11-21 14:21:45 -0300

    Put in place search inputs and functions

--- client/src/components/SearchContainer.js
+++ client/src/components/SearchContainer.js
@@ -1,11 +1,95 @@
-/* eslint-disable arrow-body-style */
 import React from 'react';
+import FormRow from './FormRow';
+import FormRowSelect from './FormRowSelect';
+import { useAppContext } from '../context/AppContext';
+import SearchContainerWrapper from '../assets/wrappers/SearchContainerWrapper';
+import { useTranslationContext } from '../context/TranslationContext';
 
 const SearchContainer = () => {
+  const {
+    isLoading,
+    search,
+    searchClassification,
+    classificationOptions,
+    searchImportance,
+    importanceOptions,
+    searchType,
+    sortOptions,
+    typeOptions,
+    handleChange,
+    clearFilters,
+  } = useAppContext();
+
+  const {
+    IMPORTANCE_LABEL,
+    CLASSIFICATION_LABEL,
+    TYPE_LABEL,
+    SEARCH_FORM,
+    SEARCH_LABEL,
+    SORT_LABEL,
+    CLEAR_FILTERS,
+  } = useTranslationContext();
+
+  const handleSearch = e => {
+    if (isLoading) return;
+    const { name, value } = e.target;
+    handleChange({ name, value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    clearFilters();
+  };
+
   return (
-    <div>
-      <h1>SearchContainer</h1>
-    </div>
+    <SearchContainerWrapper>
+      <h4>{SEARCH_FORM}</h4>
+      <form className="form-center">
+        <FormRow
+          type="text"
+          labelText={SEARCH_LABEL}
+          name="search"
+          value={search}
+          handleChange={handleSearch}
+        />
+        <FormRowSelect
+          labelText={IMPORTANCE_LABEL}
+          name="searchImportance"
+          value={searchImportance}
+          options={importanceOptions}
+          handleChange={handleSearch}
+        />
+        <FormRowSelect
+          labelText={CLASSIFICATION_LABEL}
+          name="searchClassification"
+          value={searchClassification}
+          options={classificationOptions}
+          handleChange={handleSearch}
+        />
+        <FormRowSelect
+          labelText={TYPE_LABEL}
+          name="searchType"
+          value={searchType}
+          options={typeOptions}
+          handleChange={handleSearch}
+        />
+        <FormRowSelect
+          labelText={SORT_LABEL}
+          name="sort"
+          value={sortOptions[0]}
+          options={sortOptions}
+          handleChange={handleSearch}
+        />
+        <button
+          type="button"
+          className="btn btn-block clear-btn"
+          disabled={isLoading}
+          onClick={handleSubmit}
+        >
+          {CLEAR_FILTERS}
+        </button>
+      </form>
+    </SearchContainerWrapper>
   );
 };
 
